a1811af844f34cb3d72b739c844a208c2e588a97
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-25 10:47:52 -0700

    cambiosrominavega

--- app.js
+++ app.js
@@ -26,5 +26,49 @@ let getpicoftheday = async ()=> {
 
     
 };
-
 getpicoftheday();
+const buttons = document.querySelectorAll('button'); //agarra todos los elementos que sean button
+
+const playnote = event=> //recibe un parametro llamado event
+
+{
+    const button = event.target; //recibe parametro en la funcion
+    const note = button.dataset.note; 
+    const audioId = `audio${note}`;
+    
+    const audio= document.getElementById(`audio`);
+    console.log(audio);
+    audio.play();
+}
+
+buttons.forEach
+(
+    button=> button.addEventListener('click', playnote)
+);
+
+const keyNoteDown = event =>
+{
+    const key= event.key;
+    console.log(key);
+    const button= document.querySelector(`button[data-key="${key}"]`)
+    if (button) button.click();
+} 
+const animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  const bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (const i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
+
+document.addEventListener('keydown', keyNoteDown);
